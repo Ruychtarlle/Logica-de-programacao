921612390832e4bb48faefc811c2a0ce0d426790
Ruychtarlle <ruychtarlle@hotmail.com>
2025-03-06 21:37:11 -0300

    Create aula 01.py

---
+++ aula 01.py
@@ -0,0 +1,7 @@
+#Faça um alogoritimo eu perguntando a idade da pessoa e informe se pode entrar na festa (maior de 18 anos), caso contrario, informe para pessoa que ela de ve esperar alguns anos.
+Qual sua idade ?
+20
+voce pode entrar na festa
+
+print("Olá tudo bem, seja bem vindo a Estacio, gostaria de saber sua idade?")
+input("")
